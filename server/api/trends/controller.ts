a6045711d0669b964a7efa794b3b88d956d067cc
s1f10210254 <s1f10210248@iniad.org>
2023-08-20 22:19:57 +0900

    a

--- server/api/trends/controller.ts
+++ server/api/trends/controller.ts
@@ -2,6 +2,5 @@ import { twitterUseCase } from '$/useCase/twitterUseCase';
 import { defineController } from './$relay';
 
 export default defineController(() => ({
-  // get: () => twitterUseCase.fetchTrends().then((trends) => ({ status: 200, body: trends })),
   get: () => twitterUseCase.fetchTweet().then((tweet) => ({ status: 200, body: tweet })),
 }));
--- server/api/trends/index.ts
+++ server/api/trends/index.ts
@@ -2,10 +2,6 @@ import type { DefineMethods } from 'aspida';
 import type { TweetModel } from 'commonTypesWithClient/models';
 
 export type Methods = DefineMethods<{
-  // get: {
-  //   resBody: TrendModel[];
-  // };
-
   get: {
     resBody: TweetModel[];
   };
